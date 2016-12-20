28e1e53886cec26dabd0ad57c76243fc4e4f3db0
Florin Pop <popflorin1705@yahoo.com>
2016-12-20 18:03:54 +0200

    search input and showAll checkbox

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -1,14 +1,22 @@
 var React = require('react');
 
 var TodoSearch = React.createClass({
+    handleSearch: function() {
+        var show = this.ref.show.checked;
+        var search = this.ref.search.value;
+        
+        this.props.onSearch(show, search);
+    },
     render: function() {
         return (
             <div>
                 <div>
-                    <input type="search" ref="searchText" placeholder="Search todos"/>
+                    <input type="search" ref="search" placeholder="Search todos" onChange={this.handleSearch}/>
                 </div>
                 <div>
-                    
+                    <label>
+                        <input type="checkbox" ref="show" onChange={this.handleSearch}/>
+                    </label>
                 </div>
             </div>
         );
