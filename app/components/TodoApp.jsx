d0d25f2dfdad34b53267716d6464bef49034999a
Florin Pop <popflorin1705@yahoo.com>
2016-12-20 10:26:17 +0200

    added getInitialState

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,6 +1,19 @@
 var React = require('react');
 
 var TodoApp = React.createClass({
+    getInitialState: function(){
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: "Walk the dog"
+                }, {
+                    id: 2,
+                    text: "Clean the yard"
+                }
+            ]
+        }  
+    },
     render: function (){
         return (
             <div>
