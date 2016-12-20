8a3e913b4dbcd9460a9242704f6d1ed7961f612a
Florin Pop <popflorin1705@yahoo.com>
2016-12-20 10:27:35 +0200

    initial code

---
+++ app/components/Todo.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Todo = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Test
+            </div>
+        );
+    }
+});
+
+module.exports = Todo
